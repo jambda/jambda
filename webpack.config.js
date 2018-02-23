2cf9b2c40e34888fee4cb2a3c4be48abfc70e012
COBRA\COBRA <weblayoutz@gmail.com>
2018-02-22 16:22:06 -0800

    Added eslint loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,36 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const slsw = require('serverless-webpack');
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const slsw = require('serverless-webpack')
 
 module.exports = {
-    entry: slsw.lib.entries,
-    target: 'node',
-    externals: [nodeExternals()],
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
-                'imports-loader?graphql',
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015'],
-                    },
-                },
-            ],
-        }],
-    },
-    output: {
-        libraryTarget: 'commonjs',
-        path: path.join(__dirname, '.webpack'),
-        filename: '[name].js',
-    },
-};
+	entry: slsw.lib.entries,
+	target: 'node',
+	externals: [nodeExternals()],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					'imports-loader?graphql',
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['es2015']
+						}
+					},
+					{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						use: ['eslint-loader']
+					}
+				]
+			}
+		]
+	},
+	output: {
+		libraryTarget: 'commonjs',
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].js'
+	}
+}
