0d370a3d62688665d05f0a6b73e0140d5b162a71
COBRA\COBRA <weblayoutz@gmail.com>
2018-02-22 16:20:43 -0800

    removed unused files

--- src/helper/body.js
+++
@@ -1,7 +0,0 @@
-
-
-const body = (req) => {
-    return JSON.parse(req)
-};
-
-export default body;
--- src/helper/model.js
+++
@@ -1,6 +0,0 @@
-import database from '../config/database'
-
-
-export const loadModel = (resource) => {
-    return database.define(resource.name, resource.mappings,{});
-}
