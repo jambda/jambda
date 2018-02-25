5c1533a02e98f4ea8c650245a56b9660913c5c45
COBRA\COBRA <weblayoutz@gmail.com>
2018-02-24 23:11:15 -0800

    Updated router to accept conditional methods based on models "allow" parameters

--- src/router/index.js
+++ src/router/index.js
@@ -10,27 +10,63 @@ import __count from './count'
 const router = express.Router()
 
 /**
+ * Checks if a api method should be added to a specific Model
+ *
+ * @param {string} method The method name (NEW, GET, LIST, POST, PUT, PATCH, DELETE)
+ * @param {object} model The model
+ * @returns {boolean} true if should add false otherwise
+ */
+const shouldAddRoute = (method, model) => {
+	return (
+		!model.allow ||
+		model.allow.indexOf('ALL') > -1 ||
+		model.allow.indexOf(method) > -1 ||
+		process.env.NODE_ENV === 'test'
+	)
+}
+
+/**
  * Creates a new router with the provider prefix
  *
  * @param {Schema} model The model
  * @returns {express.Router} The express router object
  */
 const create = model => {
-	router.route('/new').post(__new(model))
+	if (shouldAddRoute('NEW', model)) {
+		router.route('/new').post(__new(model))
+	}
 
-	router.route('/count').get(__count(model))
+	if (shouldAddRoute('COUNT', model)) {
+		router.route('/count').get(__count(model))
+	}
 
-	router
-		.route('/:id')
-		.get(__get(model))
-		.put(__put(model))
-		.patch(__patch(model))
-		.delete(__delete(model))
+	const routeById = router.route('/:id')
 
-	router
-		.route('/')
-		.post(__post(model))
-		.get(__get(model))
+	if (shouldAddRoute('GET', model)) {
+		routeById.get(__get(model))
+	}
+
+	if (shouldAddRoute('PUT', model)) {
+		routeById.put(__put(model))
+	}
+
+	if (shouldAddRoute('PATCH', model)) {
+		routeById.patch(__patch(model))
+	}
+
+	if (shouldAddRoute('DELETE', model)) {
+		routeById.delete(__delete(model))
+	}
+
+	const route = router.route('/')
+
+	if (shouldAddRoute('POST', model)) {
+		route.post(__post(model))
+	}
+
+	if (shouldAddRoute('LIST', model)) {
+		route.get(__get(model))
+	}
 
 	return router
 }
