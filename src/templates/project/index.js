3425bdfedf74675428fae172c30cc3260f4ddd43
COBRA\COBRA <weblayoutz@gmail.com>
2018-03-30 12:06:09 -0700

    Moved data-api related files to it's own package

--- src/templates/project/index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import Jambda from 'jambda'
-
-import userModel from './models/user.model'
-import sourceModel from './models/source.model'
-
-export const handler = Jambda('rethinkdb', [userModel, sourceModel])
